139c05636fb777aabae2e2139830ace1a6094518
Tyler Nijmeh <tylernij@gmail.com>
2023-05-13 16:43:35 -0700

    Setup
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ daemon.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+if [[ -z "$RTSP_URI" ]]; then
+    echo "[E] No RTSP_URI environmental variable set."
+    exit 1
+fi
+
+# How long each clip is in seconds.
+SEGMENT_SECONDS="${SEGMENT_SECONDS:-"300"}"
+
+# Where to store video segments.
+SEGMENT_DIR="${SEGMENT_DIR:-"segments/"}"
+
+# How often to update the live screenshot.
+LIVE_SCREENSHOT_FREQ="${LIVE_SCREENSHOT_FREQ:-"5"}"
+
+# Live screenshot file path.
+LIVE_SCREENSHOT_PATH="${LIVE_SCREENSHOT_PATH:-"live_screenshot.png"}"
+
+mkdir -p "$SEGMENT_DIR"
+
+periodic_recording() {
+    ffmpeg \
+        -hide_banner \
+        -y \
+        -loglevel error \
+        -rtsp_transport tcp \
+        -use_wallclock_as_timestamps 1 \
+        -i "$RTSP_URI" \
+        -codec copy \
+        -f segment \
+        -reset_timestamps 1 \
+        -segment_time "$SEGMENT_SECONDS" \
+        -segment_format mkv \
+        -segment_atclocktime 1 \
+        -strftime 1 \
+        "$SEGMENT_DIR/%Y-%m-%d+T%H-%M-%S.mkv"
+}
+
+live_screenshot() {
+    while true; do
+        ffmpeg \
+            -hide_banner \
+            -y \
+            -loglevel error \
+            -rtsp_transport tcp \
+            -use_wallclock_as_timestamps 1 \
+            -i "$RTSP_URI" \
+            -vframes 1 \
+            "$LIVE_SCREENSHOT_PATH"
+        echo "[I] Updated live preview: $(date)"
+        sleep "$LIVE_SCREENSHOT_FREQ"
+    done
+}
+
+periodic_recording &
+live_screenshot &
